47d8721cbb4ccd02664f9066596220b806c2b554
Damelia Coleman <dameliac@outlook.com>
2025-03-16 07:08:10 -0500

    changed database name

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # Initialize MySQL
-#mysql = SQLAlchemy(app)
+mysql = SQLAlchemy(app)
 
 # Import views (routes) after initializing app and extensions
 from app import views
